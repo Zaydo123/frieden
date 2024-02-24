ceb9c00515ccdf8013ecd75c867bd052e806d01d
Zayd Alzein <zaydalzein@gmail.com>
2024-02-24 00:32:31 -0600

    donation ui + ui fix

--- src/routes/components/row1/Row1.svelte
+++ src/routes/components/row1/Row1.svelte
@@ -1,7 +1,7 @@
 <section class="m-auto">
     <div class="bg-tertiary-900 py-10 px-3 lg:px-8 lg:py-16">
         <h2 class="text-success-50 text-5xl sm:mb-10 text-left sm:text-center font-bold selection:bg-warning-500">About Us</h2>
-        <p class="sm:text-xl `sm:text-center sm:w-4/5 md:w-3/4 md:text-center lg:w-3/4 m-auto text-secondary-300 selection:bg-tertiary-500 selection:text-warning-50">Established in 2022, the Frieden Foundation, deriving its name from the German word for 'peace', has been steadfast in its commitment to living up to its name. With a global network of dedicated members and volunteers, our efforts have left a significant mark across various communities. Our projects, fueled by passion and unwavering dedication, focus on creating meaningful, positive change worldwide. We invite you to join us on this noble journey towards global peace and harmony.</p>
+        <p class="sm:text-xl `sm:text-center sm:w-4/5 md:w-3/4 md:text-center lg:w-3/4 m-auto text-secondary-300 selection:bg-tertiary-500 selection:text-warning-50">Established in 2022, the Frieden Foundation, deriving its name from the German word for "peace", has been steadfast in its commitment to living up to its name. With a global network of dedicated members and volunteers, our efforts have left a significant mark across various communities. Our projects, fueled by passion and unwavering dedication, focus on creating meaningful, positive change worldwide. We invite you to join us on this noble journey towards global peace and harmony.</p>
         <div class="section-line"></div>
     </div>
 </section>
--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,29 +1,90 @@
-<!-- Donate Page -->
-<!--    Your generous contribution has the power to make a lasting impact on the lives of refugees and victims around the world. With your support, we can provide critical humanitarian aid and hope to those most in need. Every donation, no matter the size, contributes to our mission of providing assistance, restoring dignity, and fostering resilience among displaced communities.-->
+<script>
+	import { RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
 
 
-<div class="big-container bg-tertiary-900">
+    let pricePresetsOneTime = [10, 20, 50, 100];
+    let pricePresetsMonthly = [5, 10, 20, 50];
+
+    let isCustomAmount = false;
+    let contributionFrequency = 0; //0 representing one time, 1 representing monthly
+
+    let contribution = 0;
+
+
+
+</script>
+
+
+<div class="m-auto pb-10 w-4/5 bg-tertiary-900 rounded">
+
+    <h1 class="text-4xl pt-10 text-center font-bold text-primary-50">Donate</h1>
     
-    <div class="container mx-auto p-10">
-        <h1 class="text-white text-center text-4xl font-bold p-10">Donate</h1>
-        
-        <h1></h1>
 
+    <div class="m-auto mt-0 w-4/5">
+        <p class="text-center text-secondary-300 text-sm mt-3">Your donation will help us in our mission to create a better world for <span class="text-red-500">everyone.</span></p>
+    </div>
+
+    <!--onetime vs monthly select-->
+    <div class="m-auto mt-5 mb-4 w-3/4 flex justify-center gap-2 text-center">
+    <RadioGroup background="bg-tertiary-700" border="border-2 border-primary-600" hover="hover:bg-red-100">
+        <RadioItem bind:group={contributionFrequency} name="justify" value={0} fill="bg-red-500" >One Time</RadioItem>
+        <RadioItem bind:group={contributionFrequency} name="justify" value={1} fill="bg-red-500" >Monthly</RadioItem>
+    </RadioGroup>
+    </div>
+
+
+    <!-- quick donate buttons -->
+    <div class="m-auto flex justify-center gap-2 pb-10 text-center w-3/4 flex-wrap">
+
+        {#if contributionFrequency === 0}
+            {#each pricePresetsOneTime as price}
+                <button class="text-primary-50 bg-secondary-500 w-20 h-8 rounded-xl" on:click={() => { isCustomAmount = false; contribution = price; } }>
+                    <p class="text-center">${price}</p>
+                </button>
+            {/each}
+        {:else}
+            {#each pricePresetsMonthly as price}
+                <button class="text-primary-50 bg-secondary-500 w-20 h-8 rounded-xl" on:click={() => { isCustomAmount = false; contribution = price; } }>
+                    <p class="text-center">${price}</p>
+                </button>
+            {/each}
+        {/if}
+        
+        {#if isCustomAmount}
+            <input type="number" class="text-primary-50 bg-secondary-500 w-20 h-8 p-2 rounded-xl" bind:value={contribution}>
+        {:else}
+        
+        <button class="other text-primary-50 bg-secondary-500 w-20 h-8 rounded-xl" on:click={() => isCustomAmount = !isCustomAmount}>
+            <p class="text-center">Other</p>
+        </button>
+
+        {/if}
+        
 
     </div>
+
+    <!-- fake receipt look -->
+    <div class="m-auto w-3/4 bg-secondary-500 rounded p-5">
+        <h2 class="text-primary-50 text-2xl font-bold">Your Donation</h2>
+        <div class="flex justify-between mt-5">
+            <p class="text-primary-50">Donation Amount</p>
+            <p class="text-primary-50">$10</p>
+        </div>
+        <div class="flex justify-between mt-5">
+            <p class="text-primary-50">Donation Type</p>
+            {#if contributionFrequency === 0}
+            <p class="text-primary-50">One Time</p>
+            {:else}
+            <p class="text-primary-50">Monthly</p>
+            {/if}
+        </div>
+        <div class="flex justify-between mt-5">
+            <p class="text-primary-50">Total</p>
+            <p class="text-primary-50">${contribution}</p>
+        </div>
+        <button class="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-5">Donate</button>  
+    </div>
+
+
+    
 </div>
-
-<style lang="postcss">
-
-    .big-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        height: 100vh;
-    }
-
-    .container {
-        max-width: 1200px;
-    }
-
-</style>
